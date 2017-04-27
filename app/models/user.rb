8a6681a54aa81faac33eb21c33c7612e97446a2b
r3dcrosse <mrdavidwayman@gmail.com>
2017-04-27 15:42:16 -0700

    npm and babel setup

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveRecord::Base
-  validates_presence_of :name
-
-  has_many :scans
-end
--- config.ru
+++
@@ -1,5 +0,0 @@
-Dir.chdir(File.expand_path('..', __FILE__))
-
-require './app'
-
-run BaaahsOrg::App
